4e3bb4d77c09d54192116b0e00ff159f467ccca9
COZYkrish <cozykrish2916@gmail.com>
2026-03-20 19:22:04 +0530

    Day 2 - Commit 2: WebSocket endpoint created

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Backend running 🚀"}
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Echo: {data}")
